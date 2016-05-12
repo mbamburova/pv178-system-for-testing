9c1a7da1e10bd8f4623117050abbcca48b488c80
Michaela Bamburova <michaela.bamburova@gmail.com>
2016-05-12 13:43:58 +0200

    added WEB project

---
+++ WEB/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using BL;
+using Castle.Windsor;
+using WEB;
+
+namespace WEB {
+    public class MvcApplication : HttpApplication {
+        private static IWindsorContainer container;
+
+        protected void Application_Start() {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BootstrapContainer();
+        }
+
+        private static void BootstrapContainer() {
+            container = new WindsorContainer();
+            container.Install(new MvcInstaller());
+            container.Install(new Installer());
+
+            var controllerFactory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+    }
+}
