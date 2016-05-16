2bed79a332aab3bdc209d6f23cc07000aca488a9
Michaela Bamburova <michaela.bamburova@gmail.com>
2016-05-16 13:18:16 +0200

    init home page

---
+++ WEB/App_Start/Startup1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WEB.App_Start.Startup1))]
+
+namespace WEB.App_Start {
+    public class Startup1 {
+        public void Configuration(IAppBuilder app) {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
