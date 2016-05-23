e1a9564232d6b5bd56b3a50fd14723353fc2864f
Michaela Bamburova <michaela.bamburova@gmail.com>
2016-05-23 19:22:14 +0200

    web layer with untracked files

---
+++ WEB/Controllers/StudentGroupController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL.DTOs;
+using BL.Facades;
+using WEB.Models;
+
+namespace WEB.Controllers
+{
+    public class StudentGroupController : Controller {
+
+        private readonly StudentGroupFacade studentGroupFacade;
+
+        public StudentGroupController(StudentGroupFacade studentGroupFacade) {
+            this.studentGroupFacade = studentGroupFacade;
+        }
+
+        public ActionResult StudentGroups() {
+            var studentGroupViewModel = new StudentGroupViewModel() {
+                StudentGroups = studentGroupFacade.GetAllStudentGroups()
+            };
+            return View("StudentGroups", studentGroupViewModel);
+        }
+    }
+}
